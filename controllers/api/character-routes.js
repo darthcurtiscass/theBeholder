985a022e43f08b4d3de643ab4b0ab14a3273c2a2
Max Tribble <maxtribble@live.com>
2023-01-26 20:35:57 -0700

    adding routes for campaign and characters

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const Character = require('../../models/Character');
+const auth = require('../../utils/auth')
 
+    router.get('/', auth, async (req, res) => {
+        try {
+            const characterData = await Character.findAll()
+            const characters = characterData.map((character) => character.get({ plain : true}))
+            res.status(200).json(characters)
+        } catch (err) {
+            res.status(500).json({message:'an error occurred, please try again.'})
+        }
+    }
+
+    )
 module.exports = router;
--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -2,6 +2,21 @@
 //Use axios to get data from character-routes
 //render profile.handlebars
 const router = require('express').Router();
+const auth = require('../utils/auth');
+const axios = require('axios')
 
+router.get('/profile', auth, async (req, res) => {
+    try {
+        axios.get('/api/campaigns').then(campaigns => {
+            res.render('profile', { campaigns })
+        })
+        axios.get('/api/characters').then(characters => {
+            res.render('profile', { characters })
+        })
+        
+    } catch (err) {
+        res.status(500).json({message:'an error occurred, please try again.'})
+    }
+})
 
 module.exports = router;
