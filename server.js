7e7aed125e96f0968f965ba70a07b83b58cfb92f
Max Tribble <maxtribble@live.com>
2023-01-28 15:42:17 -0700

    fixed the campaign route put request, added routes for posts, added homepage routes, updated posts model to show date automatically

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3001;
 
 const sessionSettings = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
