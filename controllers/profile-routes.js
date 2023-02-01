02ace87bf45c1c56b8d437b90f25cc8980b79a8c
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-01-31 22:17:00 -0700

    Max Tribble and I added profile route and home route to render the profile and home pages.

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -3,7 +3,43 @@ const router = require('express').Router();
 const auth = require('../utils/auth');
 const { Character, Campaign, User } = require('../models');
 //get all characters belonging to a specific user
+
 router.get('/:user_id', async (req, res) => {
+    try {
+        const profileData = await User.findOne({
+            where: {
+                user_id: req.params.user_id
+            },
+            include: [
+                {
+                    model: Campaign,
+                    attributes: [
+                        'name',
+                        'quest',
+                        'party_size',
+                    ]
+                },
+                {
+                    model: Character,
+                    attributes: [
+                        'name',
+                        'race',
+                        'class',
+                    ]
+                }
+            ]
+        })
+        const myProfile = profileData.map((viewProfile) => viewProfile.get({ plain: true }))
+        res.status(200).json(myProfile)
+        res.render('profile', { myProfile });
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message:'an error occurred, please try again.'})
+    }
+});
+
+router.get('characters/:user_id', async (req, res) => {
     try {
         const userCharacters = await Character.findAll({
             where: {
