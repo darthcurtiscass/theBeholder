be98e44f812c1f95b0105f6dafe63dce64f0aace
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-02-06 23:05:30 -0700

    added additional p5 eyes to page

---
+++ public/js/sketch.js
@@ -0,0 +1,57 @@
+let e1, e2, e3, e4, e5, e6, e7, e8;
+
+function setup() {
+  createCanvas(264, 300);
+  noStroke();
+  e1 = new Eye(50, 50, 90);
+  e2 = new Eye(132, 150, 150);
+  e3 = new Eye(210, 250, 90);
+  e4 = new Eye(210, 50, 90);
+  e5 = new Eye(50, 250, 90);
+  // e5 = new Eye(25, 90, 10);
+  // e6 = new Eye(16, 40, 15);
+  // e7 = new Eye(90, 90, 10);
+  // e8 = new Eye(200, 395, 18);
+}
+
+function draw() {
+  background(175, 142, 8);
+  e1.update(mouseX, mouseY);
+  e2.update(mouseX, mouseY);
+  e3.update(mouseX, mouseY);
+  e4.update(mouseX, mouseY);
+  e5.update(mouseX, mouseY);
+  // e6.update(mouseX, mouseY);
+  // e7.update(mouseX, mouseY);
+  // e8.update(mouseX, mouseY);
+  e1.display();
+  e2.display();
+  e3.display();
+  e4.display();
+  e5.display();
+  // e6.display();
+  // e7.display();
+  // e8.display();
+}
+
+function Eye(tx, ty, ts) {
+  this.x = tx;
+  this.y = ty;
+  this.size = ts;
+  this.angle = 0;
+
+  this.update = function(mx, my) {
+    this.angle = atan2(my - this.y, mx - this.x);
+  };
+
+  this.display = function() {
+    push();
+    translate(this.x, this.y);
+    fill(255);
+    ellipse(0, 0, this.size, this.size);
+    rotate(this.angle);
+    fill(300, 36, 300);
+    ellipse(this.size / 5, 0, this.size / 2, this.size / 2);
+    pop();
+  };
+}
